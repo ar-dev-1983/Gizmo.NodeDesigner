e12f875cd28fe4e4d07576446152be187f2de52c
ar-dev-1983 <60426351+ar-dev-1983@users.noreply.github.com>
2021-01-09 18:52:19 +0300

    Math Nodes, and another Theme variant
    Пока не определеился с окончательным визуальным стилем, поэтому вношу изменения наобум.
    
    Добавил математические узлы сложения, деления, умножения.
    
    Редактор и сопутсвующий код пока далек от работоспособности, особо ничего не поменялось. Пишу в свободное время, поэтому все медленно

---
+++ Gizmo.NodeDesigner/Gizmo.NodeDesigner/Nodes/Math Nodes/MathAddNode.cs
@@ -0,0 +1,41 @@
+﻿using Gizmo.NodeFramework;
+using Gizmo.WPF;
+using System;
+
+namespace Gizmo.NodeDesigner
+{
+    public class MathAddNode : Node
+    {
+        public MathAddNode() : base()
+        {
+            NodeStyle = NodeStyleEnum.Minimalistic;
+            NodeName = "Add";
+            NodeCategory = "Math nodes";
+            NodeDescription = "Сумма всех входов.\nВсе входы и выход по-умолчанию Decimal, для изменения типов входов и выходов используйте настройки.";
+            UseIcon = true;
+            Icon = "m6.8076 18.121v-4.2426h7.0711v-7.0711h4.2426v7.0711h7.0711v4.2426h-7.0711v7.0711h-4.2426v-7.0711z";
+            UseOutputs = true;
+            UseInputs = true;
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            AddVariable(new Variable() { ParentId = Id, Index = 0, IsConnected = false, Name = "Out", Value = 0.0m, DefaultValue = 0.0m, VariableType = VariableType.Output, DataType = typeof(decimal) });
+            AddVariable(new Variable() { ParentId = Id, Index = 0, IsConnected = false, Name = "In0", Value = 0.0m, DefaultValue = 0.0m, VariableType = VariableType.Input,  DataType = typeof(decimal) });
+            AddVariable(new Variable() { ParentId = Id, Index = 1, IsConnected = false, Name = "In1", Value = 0.0m, DefaultValue = 0.0m, VariableType = VariableType.Input,  DataType = typeof(decimal) });
+        }
+
+        public override void Loop()
+        {
+            base.Loop();
+            try
+            {
+                Outputs[0].Value = (Decimal)Inputs[0].Value + (Decimal)Inputs[1].Value;
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
---
+++ Gizmo.NodeDesigner/Gizmo.NodeDesigner/Nodes/Math Nodes/MathDivNode.cs
@@ -0,0 +1,41 @@
+﻿using Gizmo.NodeFramework;
+using Gizmo.WPF;
+using System;
+
+namespace Gizmo.NodeDesigner
+{
+    public class MathDivNode : Node
+    {
+        public MathDivNode() : base()
+        {
+            NodeStyle = NodeStyleEnum.Minimalistic;
+            NodeName = "Div";
+            NodeCategory = "Math nodes";
+            NodeDescription = "Деление всех входов.\nВсе входы и выход по-умолчанию Decimal, для изменения типов входов и выходов используйте настройки.";
+            UseIcon = true;
+            Icon = "m16 6.8a2.6 2.6 0 0 0-2.6 2.6 2.6 2.6 0 0 0 2.6 2.6 2.6 2.6 0 0 0 2.6-2.6 2.6 2.6 0 0 0-2.6-2.6zm-9.2 7.1v4.2h18v-4.2h-18zm9.2 6a2.6 2.6 0 0 0-2.6 2.6 2.6 2.6 0 0 0 2.6 2.6 2.6 2.6 0 0 0 2.6-2.6 2.6 2.6 0 0 0-2.6-2.6z";
+            UseOutputs = true;
+            UseInputs = true;
+        }
+
+        public override void Initialize()
+        {
+            base.Initialize();
+            AddVariable(new Variable() { ParentId = Id, Index = 0, IsConnected = false, Name = "Out", Value = 0.0m, DefaultValue = 0.0m, VariableType = VariableType.Output, DataType = typeof(decimal) });
+            AddVariable(new Variable() { ParentId = Id, Index = 0, IsConnected = false, Name = "In0", Value = 0.0m, DefaultValue = 0.0m, VariableType = VariableType.Input, DataType = typeof(decimal) });
+            AddVariable(new Variable() { ParentId = Id, Index = 1, IsConnected = false, Name = "In1", Value = 0.0m, DefaultValue = 0.0m, VariableType = VariableType.Input, DataType = typeof(decimal) });
+        }
+
+        public override void Loop()
+        {
+            base.Loop();
+            try
+            {
+                Outputs[0].Value = (Decimal)Inputs[0].Value / (Decimal)Inputs[1].Value;
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
